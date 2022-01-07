9db37c7cc8b6e2519de8f7b07eacbac61b6e35c1
AdrianSadowski <sadow.adrian@gmail.com>
2022-01-07 12:05:07 +0100

    Use prettier

--- client/app.js
+++ client/app.js
@@ -1,30 +1,30 @@
 let userName;
 
 const data = {
-  loginForm: document.getElementById('welcome-form'),
-  messagesSection: document.getElementById('messages-section'),
-  messagesList: document.getElementById('messages-list'),
-  addMessageForm: document.getElementById('add-messages-form'),
-  userNameInput: document.getElementById('username'),
-  messageContentInput: document.getElementById('message-content'),
+  loginForm: document.getElementById("welcome-form"),
+  messagesSection: document.getElementById("messages-section"),
+  messagesList: document.getElementById("messages-list"),
+  addMessageForm: document.getElementById("add-messages-form"),
+  userNameInput: document.getElementById("username"),
+  messageContentInput: document.getElementById("message-content"),
 };
 
 const socket = io();
-socket.on('message', ({ author, content }) => addMessage(author, content))
+socket.on("message", ({ author, content }) => addMessage(author, content));
 
-data.loginForm.addEventListener('submit', event => login(event));
-data.addMessageForm.addEventListener('submit', event => sendMessage(event));
+data.loginForm.addEventListener("submit", (event) => login(event));
+data.addMessageForm.addEventListener("submit", (event) => sendMessage(event));
 
-function login(e){
+function login(e) {
   e.preventDefault();
 
-  if(data.userNameInput.value){
+  if (data.userNameInput.value) {
     userName = data.userNameInput.value;
-    data.loginForm.classList.remove('show');
-    data.messagesSection.classList.add('show');
-    socket.emit('join', userName);
+    data.loginForm.classList.remove("show");
+    data.messagesSection.classList.add("show");
+    socket.emit("join", userName);
   } else {
-    alert('Please write your name :)');
+    alert("Please write your name :)");
   }
 }
 
@@ -32,28 +32,26 @@ function sendMessage(e) {
   e.preventDefault();
   let messageContent = data.messageContentInput.value;
 
-  if(!messageContent.length) {
-    alert('You have to type something!');
-  }
-  else {
+  if (!messageContent.length) {
+    alert("You have to type something!");
+  } else {
     addMessage(userName, messageContent);
-    socket.emit('message', { author: userName, content: messageContent })
-    data.messageContentInput.value = '';
+    socket.emit("message", { author: userName, content: messageContent });
+    data.messageContentInput.value = "";
   }
-
-};
+}
 
 function addMessage(author, content) {
-  const message = document.createElement('li');
-  message.classList.add('message');
-  message.classList.add('message--received');
-  if(author === userName) {
-    message.classList.add('message--self'); 
-  } else if (author === 'Chat Bot') {
-    message.classList.add('message--bot');
+  const message = document.createElement("li");
+  message.classList.add("message");
+  message.classList.add("message--received");
+  if (author === userName) {
+    message.classList.add("message--self");
+  } else if (author === "Chat Bot") {
+    message.classList.add("message--bot");
   }
   message.innerHTML = `
-    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <h3 class="message__author">${userName === author ? "You" : author}</h3>
     <div class="message__content">
       ${content}
     </div>
